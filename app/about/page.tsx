0178fee17d4ab3109bbda62edf0f3cba141d9a3d
AyBee <aybee076@gmail.com>
2026-03-08 15:19:59 +0000

    About Page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import Image from "next/image";
+import Square from "@/public/hero-image-square.png";
 
 export default function About() {
   return (
-    <div>
-      <h1>This is the About Page</h1>
-    </div>
+    <main>
+      <section className="container max-w-4xl px-4 py-8 mx-auto">
+        <h1>About PrintForge</h1>
+        <div className="grid items-center gap-8 mb-12 m">
+          <div>
+            <Image src={Square} alt="Square Image" className="w-100" />
+          </div>
+          <div className="">
+            <p className="">About PrintForge</p>
+            <h2>Empowering Makers Worldwide</h2>
+            <p className="mb-4 text-gray-700">
+              Founded in 2023, PrintForge has quickly become the go-to platform
+              for 3D printing enthusiasts, makers, and professional designers to
+              share and discover amazing STL files for 3D printing.
+            </p>
+            <p className="">
+              Our mission is to foster a vibrant community where creativity
+              meets technology, enabling anyone to bring their ideas to life
+              through 3D printing.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-
+import Image from "next/image";
+import Hero from "@/public/hero-image.png";
 
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <main>
+      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl">
+        <div className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            Your go-to platform for 3D printing files
+          </p>
+          <h1 className="text-4xl font-bold md:text-5xl">
+            {" "}
+            Discover what's possible with 3D Printing
+          </h1>
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            Join our community of creators and explore a vast library of
+            user-submitted models.
+          </p>
+
+          <div className="flex gap-4">
+            <button
+              href="/3d-models"
+              className="px-6 text-black transition duration-120 bg-white border-2 border-black hover:bg-black hover:text-white"
+            >
+              Browse Models
+            </button>
+          </div>
+        </div>
+
+        <Image src={Hero} alt="Hero Image" className="w-80 md:w-120" />
+      </section>
+    </main>
   );
 }
